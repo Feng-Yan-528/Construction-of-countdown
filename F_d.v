e088a0d871dc32cec6f9843b0fe3ef038a30c5f0
Feng-Yan-528 <ml19f2y@leeds.ac.uk>
2021-05-06 18:25:01 +0100

    start
    Create a folder and initially debug the frequency divider

--- F_d.v
+++ F_d.v
@@ -9,3 +9,33 @@
  * Divide the 25MHz frequency into 1Mhz
  */
  
+ module F_d(
+ clock_25,
+ reset,
+ clock_1
+ );
+ 
+ input clock_25;
+ input  reset;
+ output reg clock_1;
+ 
+ reg  [24:0] cnt;//The register is used to store the value of the counter
+ initial cnt = 0;
+ 
+ parameter TIME=24'd1250_0000;//Clock25MHz
+ always@(posedge clock_25 or negedge reset)
+	begin
+		if(!reset)
+			begin
+				clock_1<=1'b0;
+				cnt <=25'd0;
+			end
+		else if(cnt ==TIME-1'b1)
+		begin
+			clock_1 <=~clock_1;
+			cnt <=1'b0;
+		end
+		else
+			cnt <=cnt + 1'b1;
+	end
+endmodule
---
+++ F_d.v.bak
@@ -0,0 +1,11 @@
+/*
+ * F_d
+ * ---------------------
+ * For: University of Leeds
+ * Date: 6th May 2021
+ *
+ * Description
+ * ------------
+ * Divide the 25MHz frequency into 1Mhz
+ */
+ 
